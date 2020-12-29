05e1c6408c5626987a93c816db8368ae26595758
Agustin Machiavello <agustinmachiavello@192.168.0.102>
2020-12-29 15:23:08 -0300

    Mas avances con la seccion de proyectos

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,42 @@
+var verticalTag = $('#verticalTag');
+var projectDescription1 = $('#projectDescription1');
+
+console.log(verticalTag, projectDescription1);
+
+
+function changeVerticalTag(newText, fade) {
+
+    $(verticalTag).text(newText);
+    if (fade === 'fadeIn') {
+        console.log('1')
+        $(verticalTag).fadeIn();
+    }
+    else {
+        console.log('2')
+        $(verticalTag).fadeOut();
+    }
+}
+
 $(document).ready(function(){
-    $('.project__carousel').slick({
+    $(window).scroll(function() {
+        if (isViewportVisible(projectDescription1) === true) {
+            changeVerticalTag('e-commerce', 'fadeIn');
+        }
+        else {
+            changeVerticalTag('', 'fadeOut');
+        }
     });
 });
+
+function isViewportVisible(element) {
+    var top_of_element = $(element).offset().top;
+    var bottom_of_element = $(element).offset().top + $(element).outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+        return true;
+    } else {
+        return false;
+    }
+}
